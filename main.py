d3339c404251bcac0f2a339377933fe34a75f8d7
SerovaK4 <ksusha1354@gmail.com>
2023-04-01 01:06:57 +0300

    added comments to code

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from utils import get_data, get_filtrate_data, get_last_values, get_formed_data
 
 
 def main():
-    COUNT_VALUES = 5
-    FILTERED_EMPTY_FROM = True
+    COUNT_VALUES = 5 #переменная для вывода определенного количества перевоов
+    FILTERED_EMPTY_FROM = True #переменная для вывода необходимого или обратного значения
 
-    data = get_data()
-    data = get_filtrate_data(data, FILTERED_EMPTY_FROM)
-    data = get_last_values(data, COUNT_VALUES)
-    data = get_formed_data(data)
+    data = get_data() #чтение и запись файла
+    data = get_filtrate_data(data, FILTERED_EMPTY_FROM) #Фильтрация корректных значений
+    data = get_last_values(data, COUNT_VALUES) #Сортировку данных по дате
+    data = get_formed_data(data) #запись текста в необходимой форме
 
     for row in data:
-        print(row, end='\n')
+        print(row, end='\n') #вывод текста на экран
 
 
 if __name__ == "__main__":
